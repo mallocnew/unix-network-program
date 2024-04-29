cd8f5434fd077d0d3485e1e4c8bbf921e0c7a503
jok <easytojoin@163.com>
2024-04-29 19:38:36 +0800

    pthread demo support cancel

--- pthread_demo.cc
+++ pthread_demo.cc
@@ -4,9 +4,20 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <iostream>
 
 void* task(void* arg) {
-  printf("thread\n");
+  pthread_t tid = pthread_self();
+  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+  pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
+  printf("new thread[%lu]\n", (u_int64_t)tid);
+  for (int i = 0; i < 10; ++i) {
+    printf("Thread working...\n");
+    sleep(1);
+  }
+  printf("thread[%lu] exist\n", (u_int64_t)tid);
   return NULL;
 }
 
@@ -17,7 +28,24 @@ int main(int argc, char** argv) {
     fprintf(stderr, "Error when create new thread: %s\n", strerror(ret));
     return ret;
   }
-  pthread_join(tid, NULL);
-  printf("main over\n");
+  sleep(2);
+  ret = pthread_cancel(tid);
+  if (ret != 0) {
+    fprintf(stderr, "pthread cancel\n");
+    return ret;
+  }
+  void* thread_result;
+  ret = pthread_join(tid, &thread_result);
+  if (ret != 0) {
+    fprintf(stderr, "pthread join\n");
+    return ret;
+  }
+  if (thread_result == PTHREAD_CANCELED) {
+    printf("thread was canceled, ret: %ld\n", (int64_t)thread_result);
+  } else {
+    std::cout << "thread existed, ret" << thread_result << "\n";
+  }
+  pthread_t main_tid = pthread_self();
+  printf("main[%lu] over\n", (u_int64_t)main_tid);
   return 0;
 }
